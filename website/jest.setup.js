2ed84d5d286999edcbcfb01fc0eff3366da5aa0f
hermanovevo <123115377+hermanovevo@users.noreply.github.com>
2023-08-02 17:39:25 -0400

    a

---
+++ website/jest.setup.js
@@ -0,0 +1,4 @@
+
+const fetchMock = require("jest-fetch-mock");
+
+global.fetch = fetchMock;
--- website/public/test.js
+++ website/public/test.js
@@ -1,18 +1,15 @@
 // public/test.js
-const fetchMock = require("jest-fetch-mock");
 const { getInventoryData } = require("./checkout");
-
-jest.mock("node-fetch", () => fetchMock);
 
 describe("Test 1 - getInventoryData", () => {
   beforeEach(() => {
-    fetchMock.resetMocks();
+    fetch.resetMocks();
   });
 
   test("should fetch inventory data successfully", async () => {
     // Mock the response from the server
     const mockedInventoryData = [{ name: "Item 1", quantity: 10, price: 5.99 }];
-    fetchMock.mockResponseOnce(JSON.stringify(mockedInventoryData));
+    fetch.mockResponseOnce(JSON.stringify(mockedInventoryData));
 
     // Call the function and assert the result
     const result = await getInventoryData();
@@ -21,7 +18,7 @@ describe("Test 1 - getInventoryData", () => {
 
   test("should handle errors when fetching inventory data", async () => {
     // Mock the fetch function to return a rejected Promise with an error
-    fetchMock.mockRejectOnce(new Error("Fetch error"));
+    fetch.mockRejectOnce(new Error("Fetch error"));
 
     // Call the function and assert the result
     const result = await getInventoryData();
