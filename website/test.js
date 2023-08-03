08b8b36e0cdfddb26e7639bd27d8a0cf880d3d24
hermanovevo <123115377+hermanovevo@users.noreply.github.com>
2023-08-02 20:06:02 -0400

    lessgo

--- website/test.js
+++ website/test.js
@@ -1,6 +1,11 @@
 // Test 1: getInventoryData() function
 const fetchMock = require("jest-fetch-mock");
-const { getInventoryData, updateCheckoutTable, placeOrder } = require("./public/checkout.js");
+const {
+  getInventoryData,
+  updateCheckoutTable,
+  placeOrder,
+} = require("./checkout");
+
 describe("getInventoryData()", () => {
     it("should return inventory data from the server", async () => {
       // Assuming fetch is mocked to return a sample inventory data
